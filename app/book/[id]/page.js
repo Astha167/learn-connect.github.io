1ef9f6242bfa288c6af865fdd782f78b66872701
Astha Kumari <asthakumari605@gmail.com>
2025-09-16 23:12:22 +0530

    Update page.js

--- app/book/[id]/page.js
+++ app/book/[id]/page.js
@@ -5,17 +5,17 @@ const books = [
   {
     id: 1,
     title: "Mathematics Std II",
-    embed: "https://docs.google.com/presentation/d/e/EXAMPLE_LINK/pubembed",
+    embed: "https://docs.google.com/presentation/d/e/2PACX-1vR2KPqMLI-W7T4NbO2aeGtxxiOElwxIb_uDvR-nnDs_sVlWlX5_n9CpLrCRNQf6Gg/pub?start=false&loop=false&delayms=3000",
   },
   {
     id: 2,
     title: "English Std II",
-    embed: "https://docs.google.com/presentation/d/e/EXAMPLE_LINK/pubembed",
+    embed: "https://docs.google.com/presentation/d/e/2PACX-1vTLRDSnaYBrnOriwRSHjYYx22xtyqdNy-GRBa2J5yKI159LzsLEyRULh403fmMYaQ/pub?start=true&loop=false&delayms=3000",
   },
   {
     id: 3,
     title: "Mathematics Std III",
-    embed: "https://docs.google.com/presentation/d/e/EXAMPLE_LINK/pubembed",
+    embed: "https://docs.google.com/presentation/d/e/2PACX-1vRVjhLVoLzJegU-BOEG93B2ZKirKv4UeqbD-fmj5oB9eSsxdR6h55VD0PVqif9ewg/pub?start=true&loop=false&delayms=3000",
   },
   {
     id: 4,
